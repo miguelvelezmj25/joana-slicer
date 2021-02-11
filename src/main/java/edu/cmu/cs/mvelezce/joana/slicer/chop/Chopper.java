ef2ba9fdb8f73b3a976f5ac9370c6dfd6d86672f
Miguel Velez <miguelvelez@mijecu25.com>
2021-02-11 14:11:56 -0500

    chopped density with simple thread barrier chopper in 6 secs

--- src/main/java/edu/cmu/cs/mvelezce/joana/slicer/chop/Chopper.java
+++ src/main/java/edu/cmu/cs/mvelezce/joana/slicer/chop/Chopper.java
@@ -7,6 +7,7 @@ import edu.cmu.cs.mvelezce.joana.slicer.data.ChopData;
 import edu.cmu.cs.mvelezce.joana.slicer.data.Lines;
 import edu.kit.joana.ifc.sdg.graph.SDG;
 import edu.kit.joana.ifc.sdg.graph.SDGNode;
+import edu.kit.joana.ifc.sdg.graph.chopper.barrier.SimpleThreadBarrierChopper;
 import edu.kit.joana.ifc.sdg.graph.chopper.conc.ContextSensitiveThreadChopper;
 import edu.kit.joana.util.SourceLocation;
 import org.apache.commons.io.FileUtils;
@@ -21,6 +22,10 @@ public class Chopper {
   public static final String ROOT_DIR = "./src/main/resources/chops/";
   public static final String DOT_JSON = ".json";
 
+  public static final String CONTEXT_SENSITIVE_THREAD_CHOPPER_ALGO =
+      "ContextSensitiveThreadChopper";
+  public static final String SIMPLE_THREAD_BARRIER_CHOPPER_ALGO = "SimpleThreadBarrierChopper";
+
   private static final Comparator<Lines> LINES_COMPARATOR =
       Comparator.comparingInt(Lines::getStartLineNumber).thenComparing(Lines::getEndLineNumber);
 
@@ -28,16 +33,18 @@ public class Chopper {
   private final SDG sdg;
   private final int sourceNode;
   private final int targetNode;
+  private final String algo;
 
   public Chopper(String programName) {
-    this(programName, null, -1, -1);
+    this(programName, null, -1, -1, "");
   }
 
-  public Chopper(String programName, SDG sdg, int sourceNode, int targetNode) {
+  public Chopper(String programName, SDG sdg, int sourceNode, int targetNode, String algo) {
     this.programName = programName;
     this.sdg = sdg;
     this.sourceNode = sourceNode;
     this.targetNode = targetNode;
+    this.algo = algo;
   }
 
   public static Set<ChopData> parseChopData(Collection<SDGNode> nodes) {
@@ -74,13 +81,22 @@ public class Chopper {
 
   public Collection<SDGNode> chop() {
     long start = System.currentTimeMillis();
-    edu.kit.joana.ifc.sdg.graph.chopper.Chopper chopper =
-        new ContextSensitiveThreadChopper(this.sdg);
+    edu.kit.joana.ifc.sdg.graph.chopper.Chopper chopper = this.getAlgo();
     Collection<SDGNode> chop =
         chopper.chop(this.sdg.getNode(this.sourceNode), this.sdg.getNode(this.targetNode));
     long end = System.currentTimeMillis();
-    System.out.println("Chopper in: " + ((end - start) / 1E3) + " seconds");
+    System.out.println("Chopped in: " + ((end - start) / 1E3) + " seconds");
     return chop;
+  }
+
+  private edu.kit.joana.ifc.sdg.graph.chopper.Chopper getAlgo() {
+    if (this.algo.equals(CONTEXT_SENSITIVE_THREAD_CHOPPER_ALGO)) {
+      return new ContextSensitiveThreadChopper(this.sdg);
+    }
+    if (this.algo.equals(SIMPLE_THREAD_BARRIER_CHOPPER_ALGO)) {
+      return new SimpleThreadBarrierChopper(this.sdg);
+    }
+    throw new RuntimeException("Do not know chopping algorithm " + this.algo);
   }
 
   public void saveFilesToLines(Map<String, SortedSet<Lines>> filesToLines) throws IOException {
--- src/test/java/edu/cmu/cs/mvelezce/joana/slicer/chop/ChopperTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/joana/slicer/chop/ChopperTest.java
@@ -16,15 +16,27 @@ class ChopperTest {
     String programName = "main";
     int sourceNode = 5;
     int targetNode = 21;
-    Chopper chopper = getChopper(programName, sourceNode, targetNode);
+    String algo = Chopper.SIMPLE_THREAD_BARRIER_CHOPPER_ALGO;
+    Chopper chopper = getChopper(programName, sourceNode, targetNode, algo);
     Map<String, SortedSet<Lines>> filesToLines = chopper.chopAndProcess();
     chopper.saveFilesToLines(filesToLines);
   }
 
-  private Chopper getChopper(String programName, int sourceNode, int targetNode)
+  @Test
+  void density() throws IOException {
+    String programName = "density";
+    int sourceNode = 7;
+    int targetNode = 33698;
+    String algo = Chopper.SIMPLE_THREAD_BARRIER_CHOPPER_ALGO;
+    Chopper chopper = getChopper(programName, sourceNode, targetNode, algo);
+    Map<String, SortedSet<Lines>> filesToLines = chopper.chopAndProcess();
+    chopper.saveFilesToLines(filesToLines);
+  }
+
+  private Chopper getChopper(String programName, int sourceNode, int targetNode, String algo)
       throws IOException {
     SDG sdg = getSDG(programName);
-    return new Chopper(programName, sdg, sourceNode, targetNode);
+    return new Chopper(programName, sdg, sourceNode, targetNode, algo);
   }
 
   private SDG getSDG(String programName) throws IOException {
